1fcbe17163d45805b05c4c2ce1127316f3f9cb5c
Masood2000 <masudurrehman2000@gmail.com>
2025-11-28 14:55:36 +0100

    day-0

---
+++ day-02/Day02.cpp
@@ -0,0 +1,26 @@
+//
+// Created by pcp on 11/28/2025.
+//
+
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+
+    cin.tie(nullptr);
+
+    int x;
+    cin>>x;
+
+    cout<<x+2;
+
+
+
+
+
+
+    return 0;
+}
---
+++ day-04/Day04.cpp
@@ -0,0 +1,27 @@
+//
+// Created by pcp on 11/28/2025.
+//
+
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+
+    cin.tie(nullptr);
+
+    int x;
+    cin>>x;
+
+    cout<<x+4;
+
+    cout<<"hello";
+
+
+
+
+
+    return 0;
+}
