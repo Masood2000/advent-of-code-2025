b098d45d060235f0823df6b042006670ff037fed
Masood2000 <masudurrehman2000@gmail.com>
2025-11-28 15:17:48 +0100

    day-0

--- day-00/Day00.cpp
+++ day-00/Day00.cpp
@@ -7,20 +7,39 @@
 
 using namespace std;
 
+const int N = 1000;
+
 int main() {
 
 
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int x;
-    cin>>x;
 
-    cout<<x+0;
+    vector<int> a,b;
+
+    for (int i =0;i<N;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        a.push_back(x);
+        b.push_back(y);
+    }
 
 
+    sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
 
+    map<int,int> freq;
+    for (int x:b)
+    {
+        freq[x]++;
+    }
+    long long  sum = 0;
 
+  for (int x:a) sum += (long long ) x * freq[x];
 
+    cout<<endl<<sum<<endl;
 
     return 0;
 }
--- day-01/Day01.cpp
+++ day-01/Day01.cpp
@@ -9,13 +9,8 @@ using namespace std;
 
 int main() {
 
-
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    int x;
-    cin>>x;
-
-    cout<<x+1;
 
 
 
