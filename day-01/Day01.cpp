406bf759db54eabd2c0ccd04466d34b97d7eff0c
Masood2000 <masudurrehman2000@gmail.com>
2025-12-01 16:41:27 +0100

    day-1-part-a

--- day-01/Day01.cpp
+++ day-01/Day01.cpp
@@ -1,5 +1,5 @@
 //
-// Created by pcp on 11/28/2025.
+// Created by Masood on 12/01/2025.
 //
 
 #include<iostream>
@@ -7,15 +7,60 @@
 
 using namespace std;
 
+
+//secret entrance
 int main() {
 
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int N = 4545;
+
+    vector<pair<string,int>> keys;
+
+    for (int i = 0;i<N;i++)
+    {
+        string s;
+        cin>>s;
+
+        string key = s.substr(0,1);
+        int value = stoi(s.substr(1,s.size()-1));
+
+        keys.push_back(pair(key,value));
+
+    }
 
 
 
+    int countZeros = 0;
+
+    int dialPos = 50;
+
+
+    for (auto it: keys)
+    {
+
+        int inc ;
+
+        if (it.first == "L") inc = -1;
+        else inc = 1;
+
+        dialPos += inc * it.second;
+        dialPos %= 100;
+
+        if (dialPos > 0) dialPos += 100;
+
+
+
+        if (dialPos == 0 ) countZeros++;
+    }
+
+
+    cout<<countZeros;
 
 
     return 0;
 }
+
+
+
